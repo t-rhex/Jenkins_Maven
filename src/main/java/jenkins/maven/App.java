a661a8d5ace33a4638348978f8908f5de2d2c02c
Mr Andrew <Andrew>
2021-03-08 14:46:52 -0600

    New commits

--- src/main/java/jenkins/maven/App.java
+++ src/main/java/jenkins/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is a sample Maven Application For Jenkins and Maven Integration" );
+        System.out.println( "This is a sample Maven Application For Jenkins and Maven Integration and Testing Purposes" );
     }
 }
