a5ffdf1034a932137115d07145c31b714ef501b4
Mr Andrew <Andrew>
2021-03-08 13:38:35 -0600

    Initial Commit

---
+++ src/main/java/jenkins/maven/App.java
@@ -0,0 +1,13 @@
+package jenkins.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a sample Maven Application For Jenkins and Maven Integration" );
+    }
+}
